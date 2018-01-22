a51d850ef9a09170d0d2f8106682b86d0679b9c3
gyanshah <Gyan.shah@dal.ca>
2018-01-22 15:51:02 -0400

    Sucessfully drew rectangle

---
+++ task3.py
@@ -0,0 +1,22 @@
+import turtle
+class point():
+        """point"""
+point1=point()
+point1.x=1
+point1.y=100
+bob=turtle.Turtle()
+
+
+def move_turtle(t, point):
+    t.penup()
+    t.setpos(point.x, point.y)
+    t.pendown()
+
+def draw_rect(t):
+    move_turtle(t,point1)
+    for i in range(0,4):
+        t.fd(200)
+        t.lt(90)
+draw_rect(bob)
+
+turtle.mainloop()
