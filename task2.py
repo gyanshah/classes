2e8f72454d1476fe4f4072af00325c615a46c968
gyanshah <Gyan.shah@dal.ca>
2018-01-22 15:19:20 -0400

    Sucessfully completed task 2

--- task2.py
+++ task2.py
@@ -9,10 +9,14 @@ rect.length=200
 rect.corner=point()
 rect.corner.x=0
 rect.corner.y=0
-def find_center(rect):
+"""def find_center(rect):
     p=point()
     p.x=(rect.corner.x+rect.width)/2
     p.y=(rect.corner.y+rect.length)/2
-    return p
-center=find_center(rect)
-print('The center of the rectangle is (%g,%g)'% (center.x,center.y))
+    return p"""
+
+def move_rectangle(rect,dx,dy):
+    rect.width+=dx
+    rect.length+=dy
+    return rect.width,rect.length
+print("The new coordinates of the rectangle are:(%g,%g)"%move_rectangle(rect,50,100))
