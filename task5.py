15283c14b35b8e738a50c6589befd6a601b88dae
Gyan <Gyan.shah@dal.ca>
2018-01-23 19:16:29 -0400

    Printed the current time

--- task5.py
+++ task5.py
@@ -1,11 +1,8 @@
+from datetime import datetime
 class time():
     """Time"""
 t=time()
-t.hour=0
-t.minute=20
-t.seconds=50
-
 def print_time(time):
-    print("The current time is " '%.2d:%.2d:%.2d'%(time.hour,time.minute,time.seconds))
+    print("The current time is ",datetime.now().strftime('%H:%M:%S'))
 
 print_time(t)
