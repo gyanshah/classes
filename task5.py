16aa459007d6be467ced1bf24d9ffb477c36af78
Gyan <Gyan.shah@dal.ca>
2018-01-23 11:04:25 -0400

    Completed Task 5

--- task5.py
+++ task5.py
@@ -1,11 +1,11 @@
 class time():
     """Time"""
 t=time()
-t.hour=16
+t.hour=0
 t.minute=20
 t.seconds=50
 
 def print_time(time):
-    print("The current time is ('%g':,'%g':,'%g'",% (time.hour,time.minute,time.seconds))
+    print("The current time is " '%.2d:%.2d:%.2d'%(time.hour,time.minute,time.seconds))
 
 print_time(t)
